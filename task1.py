ff709a0580238aa3a1b32acded8f2884375c79f0
Anastasia <kazantseva_nastya_95@mail.ru>
2023-01-23 14:26:49 +0500

    Done

---
+++ task1.py
@@ -0,0 +1,41 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+    
+#     *Пример:*
+    
+#     - 6782 -> 23
+#     - 0,56 -> 11
+def GetFloatValue(message):
+    value = input(message)
+    print(value[0] == '-')
+    if value[0] == '-':
+        temp = value.replace('-','', 1)
+    else:
+        temp = value
+    while not temp.replace('.','', 1).isdigit():
+        value = input('Value must be a number. Try again: ')
+    return float(value)
+
+
+def HM6():
+    user_number = GetFloatValue('Enter your number: ')
+    print(f'{user_number}', end=' -> ')
+
+    sum = 0
+    user_number = str(user_number).replace('.', '').replace('-', '')
+    for i in range (len(user_number)):
+        sum += int(user_number[i])
+    print(sum)
+
+
+
+def HM25():
+    q = list(map(int,filter(lambda x: x not in ('-', '.', ','), input('Enter your number: '))))
+    print(f'Sum = {sum(q)}')
+
+
+def main():
+    HM25()
+
+
+if __name__ == "__main__":
+    main()
---
+++ task4.py
@@ -0,0 +1,45 @@
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+# *Пример:*
+# - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import random
+
+
+def HM13():
+    array = [i / 100 * random.randint(-999, 999)
+             for i in range(random.randint(2, 10))]
+
+    print(array, end=' => ')
+
+    for i, element in enumerate(array):
+        array[i] = abs(round(element - int(element), 2))
+
+    max_element = max(array)
+    min_element = min(i for i in array if i > 0)
+
+    print(max_element - min_element)
+
+
+def HM25():
+    array = []
+
+    for i in range(random.randint(2, 10)):
+        array.append(i / 100 * random.randint(-999, 999))
+
+    print(array, end=' => ')
+
+    for i in range(len(array)):
+        array[i] = abs(round(array[i] - int(array[i]), 2))
+
+    max_element = max(array)
+    min_element = min(i for i in array if i > 0)
+
+    print(max_element - min_element)
+
+
+def main():
+    HM25()
+
+
+if __name__ == "__main__":
+    main()
